6c2048cddda4f34788992c0430c78ff252b839d2
Leesssssssss <772592971@qq.com>
2018-11-27 22:00:38 +0800

    创建

--- src/pages/created/created.js
+++ src/pages/created/created.js
@@ -2,6 +2,7 @@ import Taro, { Component } from '@tarojs/taro'
 import { View, Button, Text, Image, Input } from '@tarojs/components'
 import './created.scss'
 import { AtIcon } from 'taro-ui'
+import { Picker } from '@tarojs/components'
 import bgImg from '../../assets/image/bgImg.png'
 
 export default class Created extends Component {
@@ -10,13 +11,12 @@ export default class Created extends Component {
     // navigationBarTitleText: '创建'
   }
 
-  constructor(props) {
-    super(props)
-    // this.state = {
-    //   markers: [],
-    //   longitude: 0,
-    //   latitude: 0
-    // }
+  state = {
+    inputValue: '备忘录名称',
+    dateSel: (new Date()).getFullYear() + '-' + ((new Date()).getMonth() + 1) + '-' + (new Date()).getDate(),
+    selector: ['不重复', '每年', '每月', '每周'],
+    selectorChecked: '不重复',
+    day: 0
   }
 
   componentWillMount() {
@@ -36,6 +36,32 @@ export default class Created extends Component {
     })
   }
 
+  onDateChange = e => {
+    this.setState({
+      dateSel: e.detail.value
+    })
+    var start = (new Date()).getFullYear() + '-' + ((new Date()).getMonth() + 1) + '-' + (new Date()).getDate()
+    var start_date = new Date(start.replace(/-/g, "/"))
+    var end_date = new Date(e.detail.value.replace(/-/g, "/"))
+    var days = end_date.getTime() - start_date.getTime()
+    var day = parseInt(days / (1000 * 60 * 60 * 24))
+    this.setState({
+      day: day
+    })
+  }
+
+  onChange = e => {
+    this.setState({
+      selectorChecked: this.state.selector[e.detail.value]
+    })
+  }
+
+  onInput = e => {
+    this.setState({
+      inputValue: e.detail.value + '还有'
+    })
+  }
+
   render() {
     return (
       <View>
@@ -51,9 +77,47 @@ export default class Created extends Component {
           <Button className='saveBtn'>保存</Button>
         </View>
 
-        <View>
-          <Text>备忘录名称</Text>
-          <Input placeholder='备忘录名称'></Input>
+        <View className='inputBox'>
+          <Text className='title'>备忘录名称</Text>
+          <Input className='input' onInput={this.onInput} placeholder='请输入备忘录名称'></Input>
+        </View>
+
+        <View className='inputBox'>
+          <Text className='title'>日期</Text>
+          <Picker mode='date' onChange={this.onDateChange}>
+            <View className='picker'>
+              {this.state.dateSel}
+            </View>
+          </Picker>
+        </View>
+
+        <View className='inputBox'>
+          <Text className='title'>重复</Text>
+          <Picker mode='selector' range={this.state.selector} onChange={this.onChange}>
+            <View className='picker'>
+              {this.state.selectorChecked}
+            </View>
+          </Picker>
+        </View>
+
+        <View className='top'>
+          <View className='choose'>
+            <AtIcon className='isChoose' prefixClass='icon' value='circle' size='14' color='#fad300'></AtIcon>
+          </View>
+          <Text className='title'>置顶</Text>
+        </View>
+
+        <View className='watch'>
+          <Text className='title'>预览</Text>
+          <View className='watchBox'>
+            <View className='watchBoxItem'>
+              <Text className='name'>{this.state.inputValue}</Text>
+              <Text className='date'>{this.state.dateSel}</Text>
+            </View>
+            <View className='watchBoxItem'>
+              <Text className='day'>{this.state.day}</Text>
+            </View>
+          </View>
         </View>
       </View>
     )
